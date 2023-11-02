e0adc7a7f8f09768541dba06b6d7ccb9989b5445
saif <saif.haq99@gmail.com>
2023-11-02 19:41:53 +0000

    updated compare script

--- compare.sh
+++ compare.sh
@@ -3,15 +3,29 @@
 create_symlinks() {
     local source_dir=$1
     local target_dir="/compare"
+    local console_path="$source_dir/Console"
 
-    find "$source_dir/Console" -type f -path "*/videos/*/*.mp4" | while read -r video_path; do
-        relative_path="${video_path#$source_dir/Console/}"
-        relative_path="${relative_path/videos\//}"  # Remove the 'videos/' part from the path
-        link_path="$target_dir/$relative_path"
-        
-        mkdir -p "$(dirname "$link_path")"
-        ln -sf "$video_path" "$link_path"
-    done
+    if [ -d "$console_path" ]; then
+        find "$console_path" \( -name "*.mp4" -o -name "*.mkv" -o -name "*.avi" -o -name "*.mov" -o -name "*.flv" -o -name "*.wmv" \) -type f -print0 | \
+        while IFS= read -r -d '' video_path; do
+            relative_path="${video_path#$console_path/}"
+            relative_path="${relative_path/videos\//}"
+            link_path="$target_dir/$relative_path"
+            
+            mkdir -p "$(dirname "$link_path")"
+            ln -sf "$video_path" "$link_path"
+        done
+    else
+        local last_folder_name=$(basename "$source_dir")
+        find "$source_dir" \( -name "*.mp4" -o -name "*.mkv" -o -name "*.avi" -o -name "*.mov" -o -name "*.flv" -o -name "*.wmv" \) -type f -print0 | \
+        while IFS= read -r -d '' file_path; do
+            relative_path="${file_path#$source_dir/}"
+            link_path="$target_dir/$last_folder_name/$relative_path"
+            
+            mkdir -p "$(dirname "$link_path")"
+            ln -sf "$file_path" "$link_path"
+        done
+    fi
 }
 
 if [ $# -eq 0 ]; then
@@ -22,4 +36,3 @@ fi
 for path in "$@"; do
     create_symlinks "$(realpath "$path")"
 done
-
