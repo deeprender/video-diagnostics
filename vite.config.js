cacdfa965399a892d4367c9dca67103b3c4e9727
Alex Y. Chan <alex.chan@deeprender.ai>
2024-08-30 13:38:15 +0000

    Update API server target

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://natas.zebra-moth.ts.net',
-        secure: true,
+        secure: false,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
