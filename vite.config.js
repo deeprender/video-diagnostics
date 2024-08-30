74a7c41a42483e6273ac400eb2e79f581bfa084f
Alex Y. Chan <alex.chan@deeprender.ai>
2024-08-30 15:32:55 +0100

    Add https dependency

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import http from "https";
 
 // https://vitejs.dev/config/
 export default defineConfig({
